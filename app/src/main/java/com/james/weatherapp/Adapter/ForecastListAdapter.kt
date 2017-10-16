5b9fb9a4f5174ba540f396d4a9e08406c7b5483a
jameskaron <jameskaron@126.com>
2017-10-17 02:18:58 +0800

    123
    123

---
+++ app/src/main/java/com/james/weatherapp/Adapter/ForecastListAdapter.kt
@@ -0,0 +1,22 @@
+package com.james.weatherapp.Adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by james on 2017/9/30.
+ */
+class ForecastListAdapter (val items : List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
---
+++ app/src/main/java/com/james/weatherapp/Util/Request.kt
@@ -0,0 +1,14 @@
+package com.james.weatherapp.Util
+
+import android.util.Log
+import java.net.URL
+
+/**
+ * Created by james on 2017/10/5.
+ */
+class Request (val url : String){
+    public fun run(){
+        val forecastJsonStr = URL(url).readText();
+        Log.d(javaClass.simpleName,forecastJsonStr);
+    }
+}
