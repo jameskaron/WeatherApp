1bde788cfb4ec7ee4c29f1f15b55f334317fc159
jameskaron <jameskaron@126.com>
2017-11-03 02:08:51 +0800

    James
    操作符重载

--- app/src/main/java/com/james/weatherapp/domain/Command.kt
+++ app/src/main/java/com/james/weatherapp/domain/Command.kt
@@ -10,7 +10,11 @@ public interface Command<T> {
 }
 
 data class ForecastList(val city: String, val country: String,
-                        val dailyForecast:List<Forecast>)
+                        val dailyForecast:List<Forecast>){
+    operator fun get(position:Int): Forecast = dailyForecast[position]
+    fun size (): Int = dailyForecast.size
+
+}
 
 data class Forecast(val date: String, val description: String, val high: Int,
                     val low: Int)
