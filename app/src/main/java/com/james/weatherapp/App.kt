51580ead1617ca5b00049df22da2ce6425404333
jameskaron <jameskaron@126.com>
2017-11-28 19:11:59 +0800

    1

--- app/src/main/java/com/james/weatherapp/App.kt
+++ app/src/main/java/com/james/weatherapp/App.kt
@@ -1,16 +1,16 @@
 package com.james.weatherapp
 
 import android.app.Application
+import android.database.sqlite.SQLiteOpenHelper
+import kotlin.properties.Delegates
 
 /**
  * Created by 80575749 on 2017/11/20.
  */
 class App : Application() {
     companion object {
-        private var instance : Application? = null
-        fun instance() = instance!!
+         var instance : Application by Delegates.notNull()
     }
-
     override fun onCreate() {
         super.onCreate()
         instance = this
