b46314a8fe16d8c4a5b516578e25a9c3b8924ea5
jameskaron <jameskaron@126.com>
2017-11-29 18:08:44 +0800

    1

---
+++ app/src/main/java/com/james/weatherapp/object/CityForecastTable.kt
@@ -0,0 +1,11 @@
+package com.james.weatherapp.`object`
+
+/**
+ * Created by 80575749 on 2017/11/29.
+ */
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
---
+++ app/src/main/java/com/james/weatherapp/object/DayForecastTable.kt
@@ -0,0 +1,15 @@
+package com.james.weatherapp.`object`
+
+/**
+ * Created by 80575749 on 2017/11/29.
+ */
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
