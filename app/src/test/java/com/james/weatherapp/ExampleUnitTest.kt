7776979d7281e0dbc24cbf8ef9c837a3d74ee297
jameskaron <jameskaron@126.com>
2017-12-01 19:06:13 +0800

    1

--- app/src/test/java/com/james/weatherapp/ExampleUnitTest.kt
+++ app/src/test/java/com/james/weatherapp/ExampleUnitTest.kt
@@ -3,13 +3,19 @@ package com.james.weatherapp
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class)
+ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
